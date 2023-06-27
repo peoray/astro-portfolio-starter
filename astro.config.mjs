b36fc260ece9f6c89fa9825662c475631ab0f94e
peoray <peoray@yahoo.com>
2023-06-27 23:33:56 +0100

    feat: Add theme toggle switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,19 @@
-import { defineConfig } from 'astro/config'
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
+import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import mdx from '@astrojs/mdx'
-
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), react()],
   experimental: {
-    assets: true,
-  },
-})
+    assets: true
+  }
+});
