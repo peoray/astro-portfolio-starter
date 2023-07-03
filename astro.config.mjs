975ecd63fdff5ed7748d055ab9c1ad24fa2e3719
peoray <peoray@yahoo.com>
2023-07-03 13:07:50 +0100

    feat: Add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
-import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import { SITE_URL } from './src/consts'
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    react(),
+  ],
   experimental: {
-    assets: true
-  }
-});
+    assets: true,
+  },
+})
