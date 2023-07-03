543747f4fe2ee41f07787f176232ec6e97831588
peoray <peoray@yahoo.com>
2023-07-03 15:08:43 +0100

    refactor: refactor layout header and footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,16 @@
+// site artefacts
 export const SITE_TITLE = 'Astro Portfolio Starter'
 export const SITE_URL = 'https://astro-portfolio-starter.netlify.app/'
 export const SITE_DESCRIPTION = 'Welcome to my personal website!'
 export const PAGE_DESCRIPTION =
   'A simple portfolio theme for Astro powered by Tailwind CSS'
-export const TWITTER = 'https://twitter.com/peoray_'
-export const GITHUB = 'https://github.com/peoray/astro-portfolio-starter'
 export const AUTHOR = 'Emmanuel Raymond'
 export const IMAGE = '/images/photo.png'
 export const IMAGE_ALT = 'Photo of an avatar'
+
+// social links
+export const EMAIL = 'mailto:peoray@yahoo.com'
+export const TWITTER = 'https://twitter.com/peoray_'
+export const GITHUB = 'https://github.com/peoray/astro-portfolio-starter'
+export const YOUTUBE = 'https://youtube.com'
+export const INSTAGRAM = 'https://instagram.com'
